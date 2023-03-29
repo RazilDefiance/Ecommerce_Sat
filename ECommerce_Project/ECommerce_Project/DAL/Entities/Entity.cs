746c8bf50c65f4f6aaee62aec3adae2da63e36db
JULIAN ANDRES GÓMEZ MORENO <juliangomez250755@correo.itm.edu.co>
2023-03-28 23:28:37 -0500

    Add ControllerCountries Edit Views Countries Add OrderTable on Index Conuntries

--- ECommerce_Project/ECommerce_Project/DAL/Entities/Entity.cs
+++ ECommerce_Project/ECommerce_Project/DAL/Entities/Entity.cs
@@ -7,8 +7,10 @@ namespace ECommerce_Project.DAL.Entities
     {
         public virtual Guid Id { get; set; }
 
-        public virtual string? CreateDate { get; set; }
+        [Display(Name = "Fecha Creación")]
+        public virtual DateTime? CreateDate { get; set; }
 
-        public virtual string? ModifiedDate { get; set; }
+        [Display(Name = "Fecha Modificación")]
+        public virtual DateTime? ModifiedDate { get; set; }
     }
 }
